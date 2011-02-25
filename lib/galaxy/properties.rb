a7ffac46374ccf6c3afc8a58f8ec6979c9686549
Henning Schmiedehausen <henning@ning.com>
2011-02-24 19:08:35 -0800

    - Add http-user and http-password option to fetch data from a password   protected repository.
    - Make sure that there is no hardcoded check on http, as data might be
      fetched from https repos, too

--- lib/galaxy/properties.rb
+++ lib/galaxy/properties.rb
@@ -18,23 +18,41 @@ module Galaxy
         end
 
         class Builder
-            def initialize base, log=Logger.new(STDOUT)
+            def initialize base, http_user, http_password, log=Logger.new(STDOUT)
                 @base = base
                 @log = log
+                if !http_user.nil? && !http_password.nil?
+                  @http_auth = {:http_basic_authentication =>[http_user, http_password]}
+                end
             end
 
             def build hierarchy, file_name
                 props = {}
+
                 hierarchy.split(/\//).inject([]) do |history, part|
                     history << part
                     begin
-                        url = "#{@base}#{history.join("/")}/#{file_name}"
-                        @log.debug "Fetching #{url}"
-                        open(url) do |io|
-                            Properties.parse_props io, props
+
+                      url = "#{@base}#{history.join("/")}/#{file_name}"
+                      @log.debug "Fetching #{url}"
+
+                      auth = {}
+                      begin
+                        fetch_done = true
+                        open(url, auth) do |io|
+                          Properties.parse_props io, props
                         end
+                      rescue OpenURI::HTTPError => http_err
+                        if http_err.io.status[0] == "401" && auth.empty? && !@http_auth.nil?
+                          # retry with the auth information
+                          auth = @http_auth
+                          fetch_done = false
+                        else
+                          raise http_err
+                        end
+                      end while !fetch_done
                     rescue => e
-                        @log.debug e.message
+                      @log.debug e.message
                     end
                     history
                 end
